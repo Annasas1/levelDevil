4dad10d66403a1b2a08e4a2d89c5b8e4b15a7d0f
Alexandra Varapai <alexandra.varopai@gmail.com>
2025-10-16 12:05:28 +0200

    Create YEYlockedlevels.java

---
+++ YEYlockedlevels.java
@@ -0,0 +1,9 @@
+
+/**Some comments.
+ * 
+ */
+public class YEYlockedlevels {
+    public static boolean lvl1done = false;
+    public static boolean lvl2done = false;
+    public static boolean lvl3done = false;
+}
