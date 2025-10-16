73e2c7026d0da105d5e20ca18dfbc2841de07efd
Annasas1 <153273426+Annasas1@users.noreply.github.com>
2025-10-16 22:30:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 
 //This is our Main class in which we will run all our code. (Gameloop)
+
+/** javadoc.
+ * 
+ */
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Levil devil");
