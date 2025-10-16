e1302195a20c5d263c1ad77f13570f6b8bc6c250
Annasas1 <153273426+Annasas1@users.noreply.github.com>
2025-10-16 12:01:13 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+//This is our Main class in which we will run all our code. (Gameloop)
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Levil devil");
+        GamePanel panel = new GamePanel();
+        
+
+        frame.add(panel);
+        
+        frame.setSize(1000, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        
+    }
+}
