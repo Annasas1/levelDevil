20b6aacf9038eb2b03cd4403b516bdb3728daf32
Annasas1 <153273426+Annasas1@users.noreply.github.com>
2025-10-17 20:47:44 +0200

    Create LockedLevels.java

---
+++ LockedLevels.java
@@ -0,0 +1,9 @@
+
+/**Some comments.
+ * 
+ */
+public class LockedLevels {
+    public static boolean lvl1done = false;
+    public static boolean lvl2done = false;
+    public static boolean lvl3done = false;
+}
