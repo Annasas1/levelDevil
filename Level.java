46f2b9e2aed2b1a354da8c1c6f002a85b5de2d22
Annasas1 <153273426+Annasas1@users.noreply.github.com>
2025-10-17 20:45:42 +0200

    Update Level.java

--- Level.java
+++ Level.java
@@ -1,3 +1,20 @@
-public abstract class Level {
-    
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+
+// Level.java
+public interface Level {
+    void init(Player player); // Initialize the level with the player object
+    void update(float delta, Player player, boolean jumpQueued, GameManager controller); // Core game logic
+    void draw(Graphics2D g); // Drawing logic
+    int getLevelID();
 }
