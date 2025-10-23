ebd2de643ec7bf5ac3506c26c57186039c1345e3
Alexandra Varapai <alexandra.varopai@gmail.com>
2025-10-23 14:30:02 +0200

    Delete LockedLevels.java

--- LockedLevels.java
+++
@@ -1,9 +0,0 @@
-
-/**Some comments.
- * 
- */
-public class LockedLevels {
-    public static boolean lvl1done = false;
-    public static boolean lvl2done = false;
-    public static boolean lvl3done = false;
-}
