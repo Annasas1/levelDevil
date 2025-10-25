d7c777d15b182b679f8c06106629a8b0b3ef4b4b
Annasas1 <153273426+Annasas1@users.noreply.github.com>
2025-10-25 23:21:55 +0200

    Update Level3.java

--- Level3.java
+++ Level3.java
@@ -3,7 +3,9 @@ import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import javax.swing.*;
 
-
+/**
+ * Level3 which inherits from level to create new level. 
+ */
 public class Level3 implements Level {
     // ---------------------------------------------------------------------
     // Level Specific Data (Platforms, Spikes, Door)
